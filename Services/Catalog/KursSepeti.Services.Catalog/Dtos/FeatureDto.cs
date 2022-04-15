a5425cf31d5cfa8875211886ed98630ab42532d1
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-16 01:35:05 +0300

    Create Dto Objects for Services/Catalog

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Dtos/FeatureDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Dtos
+{
+    public class FeatureDto
+    {
+        public int Duration { get; set; }
+
+    }
+}
