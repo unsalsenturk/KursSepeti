4c8d87e027eb618cf29d0bfcacbb923da3963647
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-16 06:54:55 +0300

    Create ICategoryService

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using KursSepeti.Services.Catalog.Dtos;
+using KursSepeti.Services.Catalog.Models;
+using KursSepeti.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Services
+{
+    internal interface ICategoryService
+    {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
