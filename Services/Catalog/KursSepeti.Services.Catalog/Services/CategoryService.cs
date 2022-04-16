275c44c1bd4e14660568549acefdb59dbf98e37b
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-16 05:16:05 +0300

    Add GetAll Method

--- Services/Catalog/KursSepeti.Services.Catalog/Services/CategoryService.cs
+++ Services/Catalog/KursSepeti.Services.Catalog/Services/CategoryService.cs
@@ -17,17 +17,22 @@ namespace KursSepeti.Services.Catalog.Services
 
         private readonly IMapper _mapper;
 
-        public CategoryService(IMapper mapper,IDatabaseSettings databaseSettings)
+        public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
         {
             var client = new MongoClient(databaseSettings.ConnectionString);
 
             var database = client.GetDatabase(databaseSettings.DatabaseName);
-            
+
 
             _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
             _mapper = mapper;
         }
-        
 
+        public async Task<Response<List<CategoryDto>>> GetAllAsync()
+        {
+            var categories = await _categoryCollection.Find(category => true).ToListAsync();
+
+            return Response<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categories), 200);
+        }
     }
 }
