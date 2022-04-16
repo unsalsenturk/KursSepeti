7eb08176a7b1fdff63b14900b383a7459e1fbe19
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-16 05:10:14 +0300

    Created Services/CategoryService

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Services/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using KursSepeti.Services.Catalog.Dtos;
+using KursSepeti.Services.Catalog.Models;
+using KursSepeti.Services.Catalog.Settings;
+using KursSepeti.Shared.Dtos;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Services
+{
+    internal class CategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper,IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            
+
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+        
+
+    }
+}
