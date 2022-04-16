9e3c34d713483b1fb1919a6b790ef8f78ea8d95e
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-16 04:58:51 +0300

    Rename ResponseDto

---
+++ Shared/KursSepeti.Shared/Dtos/Response.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace KursSepeti.Shared.Dtos
+{
+    public class Response<T>
+    {
+        public T Data { get; private set; }
+
+
+        #region  for use in code
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+        #endregion
+
+
+        public List<string> Errors { get; set; }
+
+        #region static factory method       
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            return new Response<T> { Errors = errors, StatusCode = statusCode, IsSuccessful = false };
+        }
+
+        public static Response<T> Fail(string error, int statusCode)
+        {
+            return new Response<T> { Errors = new List<string>() { error }, StatusCode = statusCode, IsSuccessful = false };
+        }
+        #endregion
+    }
+}
