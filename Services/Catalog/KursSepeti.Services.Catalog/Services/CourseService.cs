38471c6133888c6f47c34ac3d7c0eb51bd2ad078
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-18 02:56:20 +0300

    Created Services/CourseService

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using KursSepeti.Services.Catalog.Models;
+using KursSepeti.Services.Catalog.Settings;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Services
+{
+    internal class CourseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+
+        private readonly IMapper _mapper;
+
+        public CourseService(IMapper mapper,IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _mapper = mapper;
+        }
+    }
+}
