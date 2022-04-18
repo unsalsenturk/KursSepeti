0c8e44e33c31f4a9599853d1b19507a13e8e6199
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-18 19:40:49 +0300

    Add Delete Method CourseService

--- Services/Catalog/KursSepeti.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/KursSepeti.Services.Catalog/Services/CourseService.cs
@@ -109,5 +109,21 @@ namespace KursSepeti.Services.Catalog.Services
 
             return Response<NoContent>.Success(204);
         }
+
+        public async Task<Response<NoContent>> DeleteAsync(string id)
+        {
+            var result = await _courseCollection.DeleteOneAsync(x => x.Id == id);
+
+            if (result.DeletedCount > 0)
+            {
+                return Response<NoContent>.Success(204);
+            }
+            else
+            {
+                return Response<NoContent>.Fail("Course not found", 404);
+            }
+        }
+            
+
     }
 }
