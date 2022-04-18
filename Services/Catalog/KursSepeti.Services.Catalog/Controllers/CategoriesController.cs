d0accceb81ef80e7828dc7ccf2483891129ea979
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-19 01:12:59 +0300

    Create CategoriesController

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using KursSepeti.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+    }
+}
