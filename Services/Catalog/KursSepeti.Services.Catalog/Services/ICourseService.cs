71da8cfe3c404f9af47202cba745cf2f4ac4b850
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-18 21:32:25 +0300

    Create ICourseService

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Services/ICourseService.cs
@@ -0,0 +1,19 @@
+﻿using KursSepeti.Services.Catalog.Dtos;
+using KursSepeti.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Services
+{
+    internal interface ICourseService
+    {
+        Task<Response<List<CourseDto>>> GetAllAsync();
+        Task<Response<CourseDto>> GetByIdAsync(string id);
+        Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userid);
+        Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
+        Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
