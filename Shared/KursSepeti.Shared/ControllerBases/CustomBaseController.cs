c86b2685331b0d5a570afabf6bc177ca898ef452
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-19 00:34:21 +0300

    Add CreateActionResultInstance Method

--- Shared/KursSepeti.Shared/ControllerBases/CustomBaseController.cs
+++ Shared/KursSepeti.Shared/ControllerBases/CustomBaseController.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using KursSepeti.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KursSepeti.Shared.ControllerBases
 {
-    public class CustomBaseController  
+    public class CustomBaseController : ControllerBase
     {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
     }
 }
