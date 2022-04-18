10da94644c74bcffe70895e1d160de2f1ebc9a23
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-19 00:36:14 +0300

    Create CoursesController

---
+++ Services/Catalog/KursSepeti.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using KursSepeti.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        internal CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+    }
+}
