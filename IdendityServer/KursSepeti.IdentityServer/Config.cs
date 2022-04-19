fe0651882b9ed33e8ed742fd36c996567b00dac0
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-20 02:54:45 +0300

    Add Scope for JWT(Not User) APIs
    Catalog API
    
    PhotoStoct API
    
    User/Signup endpoint

--- IdendityServer/KursSepeti.IdentityServer/Config.cs
+++ IdendityServer/KursSepeti.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -18,7 +19,9 @@ namespace KursSepeti.IdentityServer
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-
+                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
+                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
         public static IEnumerable<Client> Clients =>
