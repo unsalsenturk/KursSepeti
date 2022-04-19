214af08229f3f42844d2bc67760cc602a2565147
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-20 02:07:38 +0300

    Add SignUp Endpoint

--- IdendityServer/KursSepeti.IdentityServer/Controllers/UserController.cs
+++ IdendityServer/KursSepeti.IdentityServer/Controllers/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KursSepeti.IdentityServer.Dtos;
+using KursSepeti.IdentityServer.Models;
+using KursSepeti.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +15,30 @@ namespace KursSepeti.IdentityServer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
     }
 }
