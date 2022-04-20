fcdd8961cebb3e110747fb000a126551f979d405
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-20 16:34:42 +0300

    Add Authentication for UserController

--- IdendityServer/KursSepeti.IdentityServer/Controllers/UserController.cs
+++ IdendityServer/KursSepeti.IdentityServer/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using KursSepeti.IdentityServer.Dtos;
 using KursSepeti.IdentityServer.Models;
 using KursSepeti.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +9,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace KursSepeti.IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
--- IdendityServer/KursSepeti.IdentityServer/Startup.cs
+++ IdendityServer/KursSepeti.IdentityServer/Startup.cs
@@ -28,6 +28,8 @@ namespace KursSepeti.IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalApiAuthentication();
+
             services.AddControllersWithViews();
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -81,6 +83,7 @@ namespace KursSepeti.IdentityServer
 
             app.UseRouting();
             app.UseIdentityServer();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
