6d186fb549bf332da847eda76d6d8557ca2c4be9
unsalsenturkk <unsalsenturkk@gmail.com>
2022-04-22 17:06:51 +0300

    Add PhotoSave Endpoint for PhotosController

--- Services/PhotoStock/KursSepeti.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/KursSepeti.Services.PhotoStock/Controllers/PhotosController.cs
@@ -1,15 +1,39 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KursSepeti.Services.PhotoStock.Dtos;
+using KursSepeti.Shared.ControllerBases;
+using KursSepeti.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace KursSepeti.Services.PhotoStock.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PhotosController : ControllerBase
+    public class PhotosController : CustomBaseController
     {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
     }
 }
