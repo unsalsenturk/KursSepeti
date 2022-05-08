cb3c2528e8d126ce8906d57d0b433eaa49d928a6
unsalsenturkk <unsalsenturkk@gmail.com>
2022-05-08 18:20:45 +0300

    Create ISharedIdentityService for Shared

---
+++ Shared/KursSepeti.Shared/Services/ISharedIdentityService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursSepeti.Shared.Services
+{
+    public interface ISharedIdentityService
+    {
+        public string GetUserId { get; }
+    }
+}
