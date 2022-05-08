eb71a4173afdc2ab0cd174bd4d177dc8a034f3d9
unsalsenturkk <unsalsenturkk@gmail.com>
2022-05-08 15:43:31 +0300

    Create BasketDto and BasketItemDto for Basket Service

---
+++ Services/Basket/KursSepeti.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
