745362df6edb6138e7a8f08d449b484b402bfaee
unsalsenturkk <unsalsenturkk@gmail.com>
2022-05-08 18:44:25 +0300

    Create BasketsController for Basket Service

---
+++ Services/Basket/KursSepeti.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,26 @@
+﻿using KursSepeti.Services.Basket.Services;
+using KursSepeti.Shared.ControllerBases;
+using KursSepeti.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : CustomBaseController
+    {
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public BasketsController(IBasketService basketService, ISharedIdentityService sharedIdentityService)
+        {
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+    }
+}
