ba00893b5e380cf6b0fc91b95aa7069738b71246
Ünsal Şentürk <unsalsenturkk@gmail.com>
2023-08-23 18:28:46 +0300

    Feat: Add Automapper in Order Service.

---
+++ Services/Order/KursSepeti.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using KursSepeti.Services.Order.Application.Dtos;
+using KursSepeti.Services.Order.Domain.OrderAggregate;
+
+namespace KursSepeti.Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
