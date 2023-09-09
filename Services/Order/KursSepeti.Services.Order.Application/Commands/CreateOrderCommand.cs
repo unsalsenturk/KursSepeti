ec4c3db21403e9ea40b00cc98918243c3603e4e4
Ünsal Şentürk <unsalsenturkk@gmail.com>
2023-09-09 16:09:06 +0300

    Feat: Add Comment in Order Service.

---
+++ Services/Order/KursSepeti.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using KursSepeti.Services.Order.Application.Dtos;
+using KursSepeti.Shared.Dtos;
+using MediatR;
+using System.Collections.Generic;
+
+namespace KursSepeti.Services.Order.Application.Commands
+{
+    public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyurId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto AddressDto { get; set; }
+    }
+}
---
+++ Services/Order/KursSepeti.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using KursSepeti.Services.Order.Application.Commands;
+using KursSepeti.Services.Order.Application.Dtos;
+using KursSepeti.Services.Order.Domain.OrderAggregate;
+using KursSepeti.Services.Order.Infrastructure;
+using KursSepeti.Shared.Dtos;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _dbContext;
+
+        public CreateOrderCommandHandler(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAdress = new Address(request.AddressDto.Province, request.AddressDto.District, request.AddressDto.Street, request.AddressDto.ZipCode, request.AddressDto.Line);
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.BuyurId, newAdress);
+
+            request.OrderItems.ForEach(x =>
+                {
+                    newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+                }
+            );
+
+            await _dbContext.Orders.AddAsync(newOrder);
+            await _dbContext.SaveChangesAsync();
+
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = newOrder.Id },200);
+        }
+
+       
+    }
+}
