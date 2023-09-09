a5b1e48aa7763d613ac0e48f03f8c2d7d8ae51b3
Ünsal Şentürk <unsalsenturkk@gmail.com>
2023-09-09 15:42:33 +0300

    Feat: Add Handler and Query, Mediatr for Order service.

---
+++ Services/Order/KursSepeti.Services.Order.Application/Handlers/GetOrdersByUserIdHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using KursSepeti.Services.Order.Application.Dtos;
+using KursSepeti.Services.Order.Application.Mapping;
+using KursSepeti.Services.Order.Application.Queries;
+using KursSepeti.Services.Order.Infrastructure;
+using KursSepeti.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KursSepeti.Services.Order.Application.Handlers
+{
+    public class GetOrdersByUserIdHandler : IRequestHandler<GetOrdersByUserIdQuery, Response<List<OrderDto>>>
+    {
+        private readonly OrderDbContext _context;
+
+        public GetOrdersByUserIdHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders.Include(x => x.OrderItems).Where(x => x.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+            {
+                return Response<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+            }
+
+            var ordersDto = ObjectMapper.Mapper.Map<List<OrderDto>>(orders);
+
+            return Response<List<OrderDto>>.Success(ordersDto, 200);
+        }
+    }
+}
---
+++ Services/Order/KursSepeti.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using KursSepeti.Services.Order.Application.Dtos;
+using KursSepeti.Shared.Dtos;
+using MediatR;
+using System.Collections.Generic;
+
+namespace KursSepeti.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
